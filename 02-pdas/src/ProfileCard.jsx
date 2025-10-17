e3b8965f3156ce1be4e22be56d91591fc6b90381
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-17 22:19:01 +0700

    Added title

--- 02-pdas/src/ProfileCard.jsx
+++ 02-pdas/src/ProfileCard.jsx
@@ -1,8 +1,8 @@
 
 
-function ProfileCard({image , name , handle , title}) {
+function ProfileCard({image , name ,  handle , title}) {
     return (
-       <div className="card">
+      <div className= "card">
         <div className="card-image">
             <figure className="image is-1by1">
                 <img src={image} alt="pda logo"/>
@@ -11,12 +11,12 @@ function ProfileCard({image , name , handle , title}) {
 
         <div className="card-content">
             <div className="media-content">
-                <p className="title is-4">Hello my name is {name}</p>
-                <p className="subtitle is-6">Handle is {handle}</p>
-                <p className="subtitle is-6">Title is {title}</p>
+                <p className="subtitle is-5">{name}</p>
+                <p className="title is-4">{title}</p>
+                <p className="subtitle is-6">{handle}</p>
             </div>
         </div>
-       </div>
+      </div>
     )
 }
 
