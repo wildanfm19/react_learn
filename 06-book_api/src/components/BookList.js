95fe30b5618b3d26bce4fc4a44cd4de2230ec3a9
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-20 19:50:12 +0700

    Create Book context testing

--- 06-book_api/src/components/BookList.js
+++ 06-book_api/src/components/BookList.js
@@ -1,13 +1,19 @@
+import { useContext } from 'react';
 import BookShow from './BookShow';
+import BooksContext from '../context/books';
 
 function BookList({ books, onDelete, onEdit }) {
+  const value = useContext(BooksContext);
   const renderedBooks = books.map((book) => {
     return (
       <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     );
   });
 
-  return <div className="book-list">{renderedBooks}</div>;
+  return <div className="book-list">
+  {value}
+  {renderedBooks}
+  </div>;
 }
 
 export default BookList;
