8b8bd9a1761ca2b5e31c3652ec999a6196805799
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-21 22:41:01 +0700

    Make Button Component

--- 07-navigations/src/App.js
+++ 07-navigations/src/App.js
@@ -1,28 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import Button from "./Button";
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button></Button>
       </div>
-      <h1>React Starter Project</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to test HMR
-        </p>
+
+       <div>
+        <Button></Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+       <div>
+        <Button></Button>
+      </div>
+
+       <div>
+        <Button></Button>
+      </div>
+
+       <div>
+        <Button></Button>
+      </div>
     </>
   );
 }
---
+++ 07-navigations/src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+   <button>Test</button>
+  )
+}
+
+export default Button
