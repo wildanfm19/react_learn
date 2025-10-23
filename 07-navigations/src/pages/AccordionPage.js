2ee2a76d00a9929389d911f687692e2842f23c45
wildanfm19 <dracko.maaruftm@gmail.com>
2025-10-23 16:43:02 +0700

    Make Accordion to the new Accordion Page

---
+++ 07-navigations/src/pages/AccordionPage.js
@@ -0,0 +1,33 @@
+
+import "./App.css";
+import Accordion from "./components/Accordion";
+
+
+
+function AccordionPage() {
+  const item = [
+    {
+      id : '1234',
+      label : 'Can I use React on a Project?',
+      content : 'You can use React on any project you want'
+    },
+    {
+      id: '12552',
+       label : 'Can I use React on a Project?',
+      content : 'You can use React on any project you want'
+    },
+    {
+      id : '52151',
+      label : 'Can I use React on a Project?',
+      content : 'You can use React on any project you want'
+    }
+  ];
+
+  return(
+     <Accordion items= {item}/>
+  )
+
+ 
+}
+
+export default AccordionPage;
